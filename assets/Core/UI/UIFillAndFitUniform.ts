0bb268b49d2e38589a45450d935c03b6b136fcf8
zhangdaxing <zhangdaxing@moqipobing.com>
2025-08-28 14:00:25 +0800

    修改适配UI

--- assets/Core/UI/UIFillAndFitUniform.ts
+++ assets/Core/UI/UIFillAndFitUniform.ts
@@ -26,18 +26,7 @@ export default class UIFillAndFitUniform extends cc.Component {
     private updateLayout() {
 
         if (cc.isValid(this.maskNode)) {
-
-            // 此函数是为了在 浏览器 中进行预览时，能够正常显示
-            // 因为在浏览器中，canvas 的大小是根据设备的像素比来确定的，而在 Cocos Creator 中，canvas 的大小是根据设计分辨率来确定的
-            // 所以在浏览器中预览时，需要将 canvas 的大小乘以设备的像素比，才能正常显示
-            let canvasSize: cc.Size = null;
-            if (CC_PREVIEW) {
-                const devicePixelRatio = cc.view.getDevicePixelRatio();
-                const originalSize = cc.view.getVisibleSize();
-                canvasSize = new cc.Size(originalSize.width * devicePixelRatio, originalSize.height * devicePixelRatio);
-            } else {
-                canvasSize = cc.view.getVisibleSize();
-            }
+            const canvasSize = this.getCanvasSize();
             this.maskNode.width = canvasSize.width;
             this.maskNode.height = canvasSize.height;
             this.maskNode.setPosition(cc.Vec2.ZERO);
@@ -47,14 +36,40 @@ export default class UIFillAndFitUniform extends cc.Component {
         if (cc.isValid(this.contentNode)) {
             const visibleSize = cc.view.getVisibleSize();
             const design = cc.view.getDesignResolutionSize();
+            // 检查设计分辨率是否有效
+            if (design.width <= 0 || design.height <= 0) {
+                console.warn('设计分辨率无效:', design);
+                return;
+            }
+
             const scaleX = visibleSize.width / design.width;
             const scaleY = visibleSize.height / design.height;
-            const unifomScale = Math.min(scaleX, scaleY);
+            const uniformScale = Math.min(scaleX, scaleY);
 
-            if (this.contentNode) {
-                this.contentNode.setScale(unifomScale);
-            }
+            //最多放大到设计尺寸
+            this.contentNode.setScale(Math.min(1, uniformScale));
             this.contentNode.setPosition(cc.Vec2.ZERO);
         }
+    }
+
+    /**
+     * 获取画布大小
+     * 在浏览器预览时需要考虑设备像素比
+     * @returns 画布大小
+     */
+    private getCanvasSize(): cc.Size {
+        let canvasSize: cc.Size = null;
+
+        // 此函数是为了在 浏览器 中进行预览时，能够正常显示
+        // 因为在浏览器中，canvas 的大小是根据设备的像素比来确定的，而在 Cocos Creator 中，canvas 的大小是根据设计分辨率来确定的
+        // 所以在浏览器中预览时，需要将 canvas 的大小乘以设备的像素比，才能正常显示
+        if (CC_PREVIEW) {
+            const devicePixelRatio = cc.view.getDevicePixelRatio();
+            const originalSize = cc.view.getVisibleSize();
+            canvasSize = new cc.Size(originalSize.width * devicePixelRatio, originalSize.height * devicePixelRatio);
+        } else {
+            canvasSize = cc.view.getVisibleSize();
+        }
+        return canvasSize;
     }
 }
