2a1034fb4152e28b85936159652e1c45f7088b8a
zhangdaxing <zhangdaxing@moqipobing.com>
2025-09-10 13:55:44 +0800

    Moudle的注册修改

--- assets/dx-framework/src/decorators/Moudle.ts
+++ assets/dx-framework/src/decorators/Moudle.ts
@@ -3,7 +3,10 @@ import ModuleManager from "../core/ModuleManager";
 
 
 export default function Module(token: string) {
-    return function <T extends { new(...args: any[]): IModule }>(ctor: T) {        
-        ModuleManager.instance.registerModuleByConstructor(token, ctor);
+    return function <T extends { new(...args: any[]): IModule }>(ctor: T) {
+        //方式一：通过构造函数注册
+        //ModuleManager.instance.registerModuleByConstructor(token, ctor);
+        //方式二：通过实例注册
+        ModuleManager.instance.registerModule(token, new ctor());
     };
 }
